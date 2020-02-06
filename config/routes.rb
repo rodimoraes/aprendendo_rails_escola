25ad939dcd5286d288a6efeaa0b92cebd88f437e
Rodrigo Moraes <m.rodrigocardoso@gmail.com>
2020-02-05 22:16:57 -0300

    bulma tela principal e alunos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :alunos
-
-  root to: 'alunos#index'
+  get 'dashboard/index'  
+  root to: 'dashboard#index'
 
 
 end
