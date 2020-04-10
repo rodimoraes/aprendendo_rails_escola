f3ec019f27634530963726f012b9c8f2556c5b11
Rodrigo Moraes <m.rodrigocardoso@gmail.com>
2020-04-10 20:22:57 -0300

    WIP Ferramentas

---
+++ app/controllers/ferramentas_controller.rb
@@ -0,0 +1,21 @@
+class FerramentasController < ApplicationController
+
+
+
+
+    def calculadora (operacao)
+        Case operacao
+        when '+'
+            return sum(num1,num2)
+        
+        
+    end
+
+
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :alunos
   resources :classes
+  resources :ferramentas
+  
   get 'dashboard/index'  
   root to: 'dashboard#index'
 
